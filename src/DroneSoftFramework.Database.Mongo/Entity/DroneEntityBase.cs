11f458eccc0fe9ce8aa2afadb4e261c2ac3e7dba
Diego Gomez <diegogomez211990@gmail.com>
2016-09-08 19:35:38 -0500

    Set up DroneEntityBase _Id to save as string in mongo db

--- src/DroneSoftFramework.Database.Mongo/Entity/DroneEntityBase.cs
+++ src/DroneSoftFramework.Database.Mongo/Entity/DroneEntityBase.cs
@@ -1,9 +1,13 @@
 ﻿using System;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
 
 namespace DroneSoftFramework.Database.Mongo.Entity
 {
     public abstract class DroneEntityBase
     {
+        [BsonId]
+        [BsonRepresentation(BsonType.String)]
         public Guid Id { get; set; }
     }
 }
